6c8a1aea46ce64906deb51a7c7c45a7cf81d1d9d
kyokoshima <dev.kyoko@gmail.com>
2014-07-02 23:02:01 +0900

    add virtual attribute on stock

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -69,6 +69,6 @@ class StocksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def stock_params
-      params.require(:stock).permit(:name, :quantity, :unit)
+      params.require(:stock).permit(:name, :quantity, :unit, :max_quantity)
     end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,3 +1,4 @@
 class Stock < ActiveRecord::Base
+	attr_accessor :max_quantity
 	validates :name, presence: true
 end
