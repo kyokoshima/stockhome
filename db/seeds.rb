9ac4092aa275db45f82057b77d401a25f012b655
kyokoshima <dev.kyoko@gmail.com>
2014-08-11 16:36:30 +0900

    add db:seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+unit_g = Unit.create(name: 'g')
+unit_ml = Unit.create(name: 'ml')
+unit_count = Unit.create(name: '個')
+
+categories = Category.create([
+	{name: '洗剤', parent_id: nil, unit: unit_g},
+	{name: 'トイレ用品', parent_id: nil, unit: unit_g},
+	{name: 'お風呂用品', parent_id: nil, unit: unit_ml},
+	{name: '日用品', parent_id: nil, unit: unit_count},
+	{name: '赤ちゃん用品', parent_id: nil, unit: unit_count},
+	])
+
+categories.each do | cat |
+	puts cat.pretty_inspect
+	if cat.errors.any?
+		puts cat.errors.full_messages
+	end
+end
